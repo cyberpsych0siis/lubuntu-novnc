0c634b118f9e0cdf9dbadf8a11699dc61a2b04b8
Philipp Gaßner <activator112233@gmail.com>
2022-03-19 01:12:13 +0100

    Update novnc.sh

--- novnc.sh
+++ novnc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#exec /sbin/setuser developer /home/developer/.vnc/vnc.sh >>/var/log/vncserver.log 2>&1
+exec /sbin/setuser developer /home/developer/.vnc/vnc.sh >>/var/log/vncserver.log 2>&1
 exec /noVNC/utils/novnc_proxy --vnc localhost:5901 --listen 0.0.0.0:80 >>/dev/null  2>&1
